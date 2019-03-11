4e3df3d67258a75cc9d23b99da55e1b6b83b94e6
Helena <lenka_bokshic@mail.ru>
2019-03-11 14:17:36 +0300

    read filename from configuration

---
+++ RoyalCar/Writer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prestige.RoyalCar
+{
+    class Writer
+    {
+        public static void printLine(String message)
+        {
+            Console.WriteLine(message);
+        }
+        public static void printArray(List<Car> cars) {
+            for (int i = 0; i < cars.Count; i++)
+            {
+                Console.WriteLine(i + 1 + ". " + cars[i].ToString());
+            }
+        }
+    }
+}
