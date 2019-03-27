320c236ab45354115dfd9b0424e1e8c485bf6648
Helena <lenka_bokshic@mail.ru>
2019-03-28 00:19:28 +0300

    Added class of RentManagement, that controll actions of customer

---
+++ RoyalCar/RentManagement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prestige.RoyalCar
+{
+    class RentManager
+    {
+        public void CheckOccupyCar(Dictionary<String, String> dictionary, List<Car> cars, String idCustomer, int index)
+        {
+            if (cars[index].IsOccupied)
+            {
+                dictionary.Add(cars[index].IdOfCar, idCustomer);
+                cars[index].IsOccupied = false;
+            }
+            else
+            {
+                throw new OccupyException("This car already is occupy ");
+            }
+        }
+
+        public void CheckRetrieveCar(Dictionary<String, String> dictionary, List<Car> cars, String idCustomer, int index)
+        {
+            if (cars[index].IsOccupied)
+            {
+                throw new OccupyException("This car already is retrieve ");
+            }
+            else
+            {
+                dictionary.Remove(cars[index].IdOfCar);
+                cars[index].IsOccupied = true;
+            }
+        }
+    }
+}
