71e1e50a60d86b7f035c125a8d5c55a4d3ed8fa8
Helena <lenka_bokshic@mail.ru>
2019-03-29 00:56:29 +0300

    Deleted block else in RentManagement

--- Prestige.RoyalCar.Client.Business/RentManagement.cs
+++ Prestige.RoyalCar.Client.Business/RentManagement.cs
@@ -10,16 +10,13 @@ namespace Prestige.RoyalCar.Client.Business
     {
         public void CheckOccupyCar(Dictionary<String, String> databaseOfOrders, List<Car> cars, String idCustomer, int index)
         {
-            if (cars[index].IsOccupied)
-            {
-                databaseOfOrders.Add(cars[index].Id, idCustomer);
-                cars[index].IsOccupied = false;
-            }
-            else
+            if (!cars[index].IsOccupied)
             {
                 throw new OccupyException("This car is already occupied ");
             }
-        }
+            databaseOfOrders.Add(cars[index].Id, idCustomer);
+            cars[index].IsOccupied = false;
+           }
 
         public void CheckRetrieveCar(Dictionary<String, String> dictionary, List<Car> cars, String idCustomer, int index)
         {
@@ -27,11 +24,8 @@ namespace Prestige.RoyalCar.Client.Business
             {
                 throw new OccupyException("This car is already retrieved ");
             }
-            else
-            {
-                dictionary.Remove(cars[index].Id);
-                cars[index].IsOccupied = true;
-            }
+            dictionary.Remove(cars[index].Id);
+            cars[index].IsOccupied = true;
         }
     }
 }
