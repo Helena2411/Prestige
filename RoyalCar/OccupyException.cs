4b39de47f2d44389db586b847f92957cf3692034
Helena <lenka_bokshic@mail.ru>
2019-03-28 17:54:59 +0300

    Added some constructors for own class of exception

--- RoyalCar/OccupyException.cs
+++ RoyalCar/OccupyException.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +12,9 @@ namespace Prestige.RoyalCar
         public OccupyException() { }
 
         public OccupyException(string message) : base(message) { }
+
+        public OccupyException(string message, Exception inner) : base(message, inner) { }
+
+        public OccupyException(SerializationInfo info, StreamingContext context) : base(info, context) { }  
     }
 }
